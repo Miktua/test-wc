8fe9e2e654f100f4b6422e80e27ab07b015118df
George <george@bykollab.com>
2022-09-09 09:52:14 +0300

    adds mainnet

--- utils/config.ts
+++ utils/config.ts
@@ -1,19 +1,19 @@
 import { AbiItem } from "web3-utils";
 import mintAbi from "../abi/abi.json";
 
-export const NET_ID = 4;
-export const CHAIN_ID = "0x4";
+export const NET_ID = 1;
+export const CHAIN_ID = "0x1";
 export const PROVIDER_ETH =
-  "https://rinkeby.infura.io/v3/66464a5382084773b10e288211f7c10e";
+  "https://mainnet.infura.io/v3/66464a5382084773b10e288211f7c10e";
 export const baseURL = "https://backend.do.bykollab.com";
 
 export const NETWORKS = {
-  "4": PROVIDER_ETH,
+  "1": PROVIDER_ETH,
 };
 
 export const CONTRACTS = {
   MINT: {
     abi: mintAbi as AbiItem[],
-    address: "0x1B9C9d4eD506a89d67e1Ccade7E0F8dcffE36679",
+    address: "0x93de8C95a35Df940C3296ea4DB1b51fB7f7cb524",
   },
 };
